bb332300b4c25bf4af150e8241e1092364c4db7c
Luis Forbes <himduncan@Luiss-MacBook-Air.local>
2023-03-02 18:08:15 -0500

    completed post functionality for car

--- supers/views.py
+++ supers/views.py
@@ -1,11 +1,13 @@
+from django.shortcuts import get_object_or_404
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
+from rest_framework import status
 from .serializers import SuperSerializer
 from .models import Super
 
 # Create your views here.
 
-@api_view(['GET'])
+@api_view(['GET', 'POST'])
 def supers_list(request):
     if request.method == 'GET':
         
@@ -18,10 +20,15 @@ def supers_list(request):
             supers = supers.filter(super__name=super_name)
         serializer = SuperSerializer(supers, many= True)
         return Response(serializer.data)
+    elif request.method == 'POST':
+        serializer = SuperSerializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+        return Response(serializer.data, status=status.HTTP_201_CREATED)
     
 @api_view(['GET'])
 def super_detail(request, pk):
-    super = get_object_or_404 (Super, pk=pk)
+    super = get_object_or_404(Super, pk=pk)
     if request.method == 'GET':
         serializer = SuperSerializer(super)
         return Response(serializer.data)
