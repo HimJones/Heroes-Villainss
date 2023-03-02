6daa15ed60efd53006df2735a136b4dc744bba20
Luis Forbes <himduncan@Luiss-MacBook-Air.local>
2023-03-01 19:19:12 -0500

    created local settings file + relocated Secret Key and Database to local_settings.

--- heroes_villains_project/settings.py
+++ heroes_villains_project/settings.py
@@ -19,10 +19,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-lx@655y2u(%2&ushp^2yme%lg_*u5e76m3(hr%78=b^%vbyr%j'
 
-# SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
 ALLOWED_HOSTS = []
@@ -69,16 +66,6 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'heroes_villains_project.wsgi.application'
 
-
-# Database
-# https://docs.djangoproject.com/en/4.1/ref/settings/#databases
-
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
 
 
 # Password validation
